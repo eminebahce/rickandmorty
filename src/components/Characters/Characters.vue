0019970308b2ed25c4e95b6b4ac82f4f063955da
EmineBC <eminebahce@gmail.com>
2020-01-18 22:06:17 +0100

    information added into the cards and navigation buttons added

--- src/components/Characters/Characters.vue
+++ src/components/Characters/Characters.vue
@@ -10,14 +10,40 @@
                 <v-card
                         color='#263238'
                         dark
+                        hover
+                        flat
+                        ripple
+                        shaped
                 >
                     <div class="d-flex flex-no-wrap justify-space-between">
                         <div>
-                            <v-card-title
-                                    v-text="character.name"
-                            >
-                            </v-card-title>
-                            <v-card-subtitle v-text="character.status"></v-card-subtitle>
+                            <v-card-title v-text="character.name"></v-card-title>
+                            <template v-if="character.status === 'Alive'">
+                                <v-chip class="ml-3" outlined pill>
+                                    {{character.status}}
+                                    <v-icon class="ml-2" color="green accent-4" left>
+                                        mdi-account-multiple-check
+                                    </v-icon>
+                                </v-chip>
+                            </template>
+                            <template v-else>
+                                <v-chip class="ml-3" outlined pill>
+                                    {{character.status}}
+                                    <v-icon class="ml-2" color="red accent-4" left>
+                                        mdi-account-remove
+                                    </v-icon>
+                                </v-chip>
+                            </template>
+                            <v-card-text>
+                                <div>Species: {{character.species}}</div>
+                                <v-divider class="mx-xl-4"></v-divider>
+                                <div>Gender: {{character.gender}}</div>
+                                <v-divider class="mx-xl-4"></v-divider>
+                                <div>Origin: {{character.origin.name}}</div>
+                                <v-divider class="mx-xl-4"></v-divider>
+                                <div>Last Location: {{character.location.name}}</div>
+                                <v-divider class="mx-xl-4"></v-divider>
+                            </v-card-text>
                         </div>
                         <v-avatar
                                 class="ma-3"
@@ -28,7 +54,20 @@
                         </v-avatar>
                     </div>
                 </v-card>
-
+            </v-col>
+        </v-row>
+        <v-row justify="center">
+            <v-col cols="12" md="2">
+                <v-btn color="#546E7A">
+                    <v-icon left>mdi-arrow-left-drop-circle-outline</v-icon>
+                    Prev
+                </v-btn>
+            </v-col>
+            <v-col cols="12" md="2">
+                <v-btn color="#546E7A">
+                    Next
+                    <v-icon right>mdi-arrow-right-drop-circle-outline</v-icon>
+                </v-btn>
             </v-col>
         </v-row>
     </v-container>
